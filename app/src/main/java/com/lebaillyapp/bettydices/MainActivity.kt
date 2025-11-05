6ed6924fafe48bdb3b467aa9e0327d40dfc1d7f3
Samuel Lebailly <sam.meusa@gmail.com>
2025-11-05 22:37:30 +0100

    feature(main/): Clean default generated code + add call to the new developed composable to test and debug.

--- app/src/main/java/com/lebaillyapp/bettydices/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/bettydices/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.lebaillyapp.bettydices.ui.composable.SpriteButtonPreview
 import com.lebaillyapp.bettydices.ui.theme.BettyDicesTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             BettyDicesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    SpriteButtonPreview( modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BettyDicesTheme {
-        Greeting("Android")
     }
 }
