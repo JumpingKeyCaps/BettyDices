b88618f56d23306f9fc1be622c9b6a4ab120520f
Samuel Lebailly <sam.meusa@gmail.com>
2025-11-07 17:30:47 +0100

    feature(ui/composable): Cleaning of the class and the code + add simple preview.

--- app/src/main/java/com/lebaillyapp/bettydices/ui/composable/SpriteButton.kt
+++ app/src/main/java/com/lebaillyapp/bettydices/ui/composable/SpriteButton.kt
@@ -11,14 +11,11 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -44,12 +41,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ChainStyle
 import com.lebaillyapp.bettydices.R
-
-import androidx.constraintlayout.compose.ConstraintLayout
-
-
 
 
 @Composable
@@ -145,12 +137,7 @@ fun SpriteButton(
 
 
 
-
-
-
-
-
-//@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
 @Composable
 fun SpriteButtonPreview(modifier: Modifier = Modifier) {
 
@@ -159,171 +146,27 @@ fun SpriteButtonPreview(modifier: Modifier = Modifier) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center){
         Row(
             modifier = Modifier
-                .align(Alignment.BottomCenter)
+                .align(Alignment.Center)
                 .padding(bottom = 36.dp)
                 .fillMaxWidth()
                 .background(Color.Black),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally)
         ) {
-
             SpriteButton(
                 resId = R.drawable.cyb_but_scoreur,
-                text = null,
+                text = "1923",
                 textSize = 30.sp,
                 fontRes = R.font.micro_regular,
                 textColor = Color.White,
-                contentDescription = "Enabled",
-                onClick = { Log.d("SpriteButton", "Clicked!") },
-                size = sizerButt,
-                enabled = true,
-                disabledAlpha = 0.4f
-            )
-
-            SpriteButton(
-                resId = R.drawable.cyb_but_bankbag,
-                text = "4136",
-                textSize = 30.sp,
-                fontRes = R.font.micro_regular,
-                textColor = Color.White,
-                contentDescription = "Enabled",
-                onClick = { Log.d("SpriteButton", "Clicked!") },
-                size = sizerButt,
-                enabled = true,
-                disabledAlpha = 0.4f
-            )
-            SpriteButton(
-                resId = R.drawable.cyb_but_bet,
-                text = null,
-                textSize = 30.sp,
-                fontRes = R.font.micro_regular,
-                textColor = Color.White,
-                contentDescription = "Enabled",
-                onClick = { Log.d("SpriteButton", "Clicked!") },
-                size = sizerButt,
-                enabled = true,
-                disabledAlpha = 0.4f
-            )
-            SpriteButton(
-                resId = R.drawable.cyb_but_dices,
-                text = "36",
-                textSize = 30.sp,
-                fontRes = R.font.micro_regular,
-                textColor = Color.White,
-                contentDescription = "Enabled",
-                onClick = { Log.d("SpriteButton", "Clicked!") },
-                size = sizerButt,
-                enabled = true,
-                disabledAlpha = 0.4f,
-                vibronicType = 999
-            )
-
-
-        }
-    }
-
-
-
-}
-
-
-@Preview(showBackground = true, backgroundColor = 0xFF000000)
-@Composable
-fun SpriteButtonConstraintPreview(modifier: Modifier = Modifier) {
-    val sizerButt = 90.dp
-    val upSpacing = 10.dp
-    val interSpacing = 5.dp
-
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color.Black),
-        contentAlignment = Alignment.BottomCenter
-    ) {
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 36.dp)
-        ) {
-            // refs pour les boutons
-            val (btn1, btn2, btn3, btn4, btnTop) = createRefs()
-            // refs pour les spacers
-            val (spacer1, spacer2, spacer3) = createRefs()
-
-            // 1 - Bank
-            SpriteButton(
-                modifier = Modifier.constrainAs(btn1) { bottom.linkTo(parent.bottom) },
-                resId = R.drawable.cyb_but_scoreur,
-                text = null,
-                onClick = { Log.d("SpriteButton", "Clicked 1!") },
-                size = sizerButt,
-                fontRes = R.font.micro_regular
-            )
-            // Spacer 1
-            Spacer(modifier = Modifier
-                .width(interSpacing)
-                .constrainAs(spacer1) { bottom.linkTo(btn1.bottom) })
-
-            // 2 - Stack
-            SpriteButton(
-                modifier = Modifier.constrainAs(btn2) { bottom.linkTo(btn1.bottom) },
-                resId = R.drawable.cyb_but_bankbag,
-                text = "4136",
-                textSize = 30.sp,
-                onClick = { Log.d("SpriteButton", "Clicked 2!") },
-                size = sizerButt,
-                fontRes = R.font.micro_regular
-            )
-            // Spacer 2
-            Spacer(modifier = Modifier
-                .width(interSpacing)
-                .constrainAs(spacer2) { bottom.linkTo(btn2.bottom) })
-
-            // 3 - Bet
-            SpriteButton(
-                modifier = Modifier.constrainAs(btn3) { bottom.linkTo(btn1.bottom) },
-                resId = R.drawable.cyb_but_bet,
-                onClick = { Log.d("SpriteButton", "Clicked 3!") },
-                size = sizerButt,
-                textSize = 30.sp,
-                fontRes = R.font.micro_regular
-            )
-            // Spacer 3
-            Spacer(modifier = Modifier
-                .width(interSpacing)
-                .constrainAs(spacer3) { bottom.linkTo(btn3.bottom) })
-
-            // 4 - Roll
-            SpriteButton(
-                modifier = Modifier.constrainAs(btn4) { bottom.linkTo(btn1.bottom) },
-                resId = R.drawable.cyb_but_dices,
-                text = "36",
-                textSize = 30.sp,
                 vibronicType = 999,
-                onClick = { Log.d("SpriteButton", "Clicked 4!") },
+                contentDescription = "Enabled",
+                onClick = { Log.d("SpriteButton", "Clicked!") },
                 size = sizerButt,
-                fontRes = R.font.micro_regular
-            )
-
-            // 5- Up Roll!
-            SpriteButton(
-                modifier = Modifier.constrainAs(btnTop) {
-                    bottom.linkTo(btn4.top, margin = upSpacing)
-                    start.linkTo(btn4.start)
-                    end.linkTo(btn4.end)
-                },
-                resId = R.drawable.cyb_but_uproll,
-                vibronicType = 999,
-                onClick = { Log.d("SpriteButton", "Clicked TOP!") },
-                size = sizerButt,
-                fontRes = R.font.micro_regular
-            )
-
-            // Chaîne horizontale centrée avec spacers
-            createHorizontalChain(
-                btn1, spacer1, btn2, spacer2, btn3, spacer3, btn4,
-                chainStyle = ChainStyle.Packed
+                enabled = true,
+                disabledAlpha = 0.4f
             )
         }
     }
 }
+
