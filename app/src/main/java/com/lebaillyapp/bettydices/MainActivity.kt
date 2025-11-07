b9a6cfdb77d1171deec15b3a650fc94f734409d3
Samuel Lebailly <sam.meusa@gmail.com>
2025-11-07 16:57:52 +0100

    feature(main/): Tweak the main activity to test the new layout rendering with constraint technique.

--- app/src/main/java/com/lebaillyapp/bettydices/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/bettydices/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.lebaillyapp.bettydices.ui.composable.SpriteButtonConstraintPreview
 import com.lebaillyapp.bettydices.ui.composable.SpriteButtonPreview
 import com.lebaillyapp.bettydices.ui.theme.BettyDicesTheme
 
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BettyDicesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SpriteButtonPreview( modifier = Modifier.padding(innerPadding))
+                    SpriteButtonConstraintPreview( modifier = Modifier.padding(innerPadding))
                 }
             }
         }
