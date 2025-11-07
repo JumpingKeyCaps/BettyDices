09c9faadf52c57607fd32ce67c8576c15f5a30e1
Samuel Lebailly <sam.meusa@gmail.com>
2025-11-07 19:06:43 +0100

    feature(main/):  Rework the main activity to now use translucent navbar(OS) et status bar + gestion system bars paddings.

--- app/src/main/java/com/lebaillyapp/bettydices/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/bettydices/MainActivity.kt
@@ -4,21 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.view.WindowCompat
 import com.lebaillyapp.bettydices.ui.composition.SpriteButtonsSectionPreview
 import com.lebaillyapp.bettydices.ui.theme.BettyDicesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.Transparent.toArgb()
+        window.navigationBarColor = Color.Transparent.toArgb()
+
         setContent {
             BettyDicesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SpriteButtonsSectionPreview( modifier = Modifier.padding(innerPadding))
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black)
+                        .systemBarsPadding()// Applique le padding système automatiquement
+                ) {
+                    SpriteButtonsSectionPreview(
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
