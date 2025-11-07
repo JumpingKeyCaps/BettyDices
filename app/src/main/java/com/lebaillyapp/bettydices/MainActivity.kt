ec1806faff1777d63d6c0e69b64943d2b1edfa9e
Samuel Lebailly <sam.meusa@gmail.com>
2025-11-07 17:33:17 +0100

    feature(main/): Clean the code + point to the composition to test.

--- app/src/main/java/com/lebaillyapp/bettydices/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/bettydices/MainActivity.kt
@@ -7,12 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.lebaillyapp.bettydices.ui.composable.SpriteButtonConstraintPreview
-import com.lebaillyapp.bettydices.ui.composable.SpriteButtonPreview
+import com.lebaillyapp.bettydices.ui.composition.SpriteButtonsSectionPreview
 import com.lebaillyapp.bettydices.ui.theme.BettyDicesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BettyDicesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SpriteButtonConstraintPreview( modifier = Modifier.padding(innerPadding))
+                    SpriteButtonsSectionPreview( modifier = Modifier.padding(innerPadding))
                 }
             }
         }
