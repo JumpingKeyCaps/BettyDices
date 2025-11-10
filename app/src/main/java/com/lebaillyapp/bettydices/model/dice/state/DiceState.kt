f0670ff95f7c28836b2ebaaec31c1c6b41c6eeb0
Samuel Lebailly <sam.meusa@gmail.com>
2025-11-10 19:14:26 +0100

    feature(model/dice/state):  Create a data model for the dice states.

---
+++ app/src/main/java/com/lebaillyapp/bettydices/model/dice/state/DiceState.kt
@@ -0,0 +1,19 @@
+package com.lebaillyapp.bettydices.model.dice.state
+
+/**
+ * États possibles d'animation d'un dé.
+ */
+enum class DiceState {
+    /**
+     * État de repos - rotation douce et calme
+     * Le dé peut être manipulé librement par l'utilisateur
+     */
+    IDLE,
+
+    /**
+     * État de lancer - rotation chaotique et rapide
+     * Le dé tourne rapidement dans toutes les directions
+     * Interaction utilisateur désactivée
+     */
+    ROLLING
+}
