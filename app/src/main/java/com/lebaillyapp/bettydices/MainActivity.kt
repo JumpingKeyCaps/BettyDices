0322c24d1b6ac32ab0e6d3442e6de9ea839e9abd
Samuel Lebailly <sam.meusa@gmail.com>
2025-11-10 19:20:46 +0100

    feature(main/):  Clean the main activity , and now use directly the game screen composition.

--- app/src/main/java/com/lebaillyapp/bettydices/MainActivity.kt
+++ app/src/main/java/com/lebaillyapp/bettydices/MainActivity.kt
@@ -3,18 +3,10 @@ package com.lebaillyapp.bettydices
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.core.view.WindowCompat
-import com.lebaillyapp.bettydices.ui.composition.SpriteButtonsSectionPreview
+import com.lebaillyapp.bettydices.ui.screen.GameScreen
 import com.lebaillyapp.bettydices.ui.theme.BettyDicesTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,19 +15,9 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         window.statusBarColor = Color.Transparent.toArgb()
         window.navigationBarColor = Color.Transparent.toArgb()
-
         setContent {
             BettyDicesTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Black)
-                        .systemBarsPadding()// Applique le padding système automatiquement
-                ) {
-                    SpriteButtonsSectionPreview(
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
+                GameScreen()
             }
         }
     }
