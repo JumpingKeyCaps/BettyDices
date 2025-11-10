98874cc099ad00b374c2e08b0d530865a74cce87
Samuel Lebailly <sam.meusa@gmail.com>
2025-11-10 19:15:23 +0100

    feature(model/dice/state):  Add a rotation states data model.

---
+++ app/src/main/java/com/lebaillyapp/bettydices/model/dice/state/RotationState.kt
@@ -0,0 +1,7 @@
+package com.lebaillyapp.bettydices.model.dice.state
+
+// Classe interne pour gérer l'état de rotation actuel d'un cube
+class RotationState(
+    var rotX: Float = 0f,
+    var rotY: Float = 0f
+)
